d9f331d78da45b1e18c8dbfaa1fcfe17c64932a1
Albert Moldovanu <albert@shouter.dk>
2022-09-27 18:35:29 +0200

    added animations & logic

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    
+    private Animator animator;
+    private String cameraTag = "Camera";
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(cameraTag))
+        {
+            animator.SetBool("isDancing", true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag(cameraTag))
+        {
+            gameObject.transform.LookAt(other.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag(cameraTag))
+        {
+            animator.SetBool("isDancing", false);
+            animator.SetBool("isWaving", true);
+        }
+    }
+}
