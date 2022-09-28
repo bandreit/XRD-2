2d12dc0289ce06eacebbad6a3085a7a08c89ad99
Albert Moldovanu <albert@shouter.dk>
2022-09-28 19:34:24 +0200

    Script modified

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -6,10 +6,14 @@ using UnityEngine.UI;
 
 public class AnimationController : MonoBehaviour
 {
-    
     private Animator animator;
     private String cameraTag = "Camera";
     private Canvas canvas;
+
+    private bool rabbitQuiz = false;
+    private bool pandaQuiz = false;
+    private bool frogQuiz = false;
+    private bool racoonQuiz = false;
 
     private void Start()
     {
@@ -17,16 +21,34 @@ public class AnimationController : MonoBehaviour
         GameObject canvasObj = GameObject.FindGameObjectWithTag("Canvas");
         canvas = canvasObj.GetComponent<Canvas>();
     }
-    
+
     private void OnTriggerEnter(Collider other)
     {
+        if (rabbitQuiz) return;
+        // && (pandaQuiz || frogQuiz || rabbitQuiz || rabbitQuiz)
         if (other.gameObject.CompareTag(cameraTag))
         {
             animator.SetBool("isDancing", true);
             canvas.enabled = true;
+            rabbitQuiz = true;
+            // switch (this.gameObject.tag)
+            // {
+            //     case "Panda":
+            //         pandaQuiz = false;
+            //         break;
+            //     case "Frog":
+            //         frogQuiz = false;
+            //         break;
+            //     case "Racoon":
+            //         racoonQuiz = false;
+            //         break;
+            //     case "Rabbit":
+            //         rabbitQuiz = false;
+            //         break;
+            // }
         }
     }
-    
+
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag(cameraTag))
@@ -34,5 +56,33 @@ public class AnimationController : MonoBehaviour
             animator.SetBool("isDancing", false);
             animator.SetBool("isWaving", true);
         }
+    }
+
+
+    public void FirstButton()
+    {
+        Debug.Log("Bralaaaaa");
+        // GameObject button = GameObject.FindGameObjectWithTag("Button1");
+        // Image image = button.GetComponent<Image>();
+        // image.color = new Color(255, 0, 0);
+        canvas.enabled = false;
+    }
+
+    public void SecondButton()
+    {
+        Debug.Log("Button 2");
+        canvas.enabled = false;
+    }
+
+    public void ThirdButton()
+    {
+        Debug.Log("Button 3");
+        canvas.enabled = false;
+    }
+
+    public void FourthButton()
+    {
+        Debug.Log("Button 4");
+        canvas.enabled = false;
     }
 }
